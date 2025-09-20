fbe01520cca5cce5138ec7289b270299a2b6c11f
SOIT <SOIT@ACAD-FC-237-L.mapua.edu.ph.local>
2025-09-20 20:40:18 +0800

    Fix StartUpYourNewLife model table name and simplify MemberForm to remove Section import errors

--- app/Filament/Resources/Members/Schemas/MemberForm.php
+++ app/Filament/Resources/Members/Schemas/MemberForm.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Members\Schemas;
 use App\Models\MemberType;
 use App\Models\Status;
 use App\Models\G12Leader;
-use Filament\Forms;
 use Filament\Schemas\Schema;
 
 class MemberForm
@@ -14,50 +13,7 @@ class MemberForm
     {
         return $schema
             ->components([
-                Forms\Components\Section::make('Personal Information')
-                    ->schema([
-                        Forms\Components\TextInput::make('first_name')
-                            ->label('First Name')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('middle_name')
-                            ->label('Middle Name')
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('last_name')
-                            ->label('Last Name')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\DatePicker::make('birthday')
-                            ->label('Birthday'),
-                        Forms\Components\TextInput::make('email')
-                            ->label('Email')
-                            ->email()
-                            ->required()
-                            ->unique(ignoreRecord: true)
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('phone')
-                            ->label('Phone')
-                            ->tel()
-                            ->maxLength(255),
-                        Forms\Components\Textarea::make('address')
-                            ->label('Address')
-                            ->rows(3),
-                    ])
-                    ->columns(2),
-                Forms\Components\Section::make('Assignment Information')
-                    ->schema([
-                        Forms\Components\Select::make('member_type_id')
-                            ->label('Member Type')
-                            ->options(MemberType::all()->pluck('name', 'id'))
-                            ->required(),
-                        Forms\Components\Select::make('status_id')
-                            ->label('Status')
-                            ->options(Status::all()->pluck('name', 'id')),
-                        Forms\Components\Select::make('g12_leader_id')
-                            ->label('G12 Leader')
-                            ->options(G12Leader::all()->pluck('name', 'id')),
-                    ])
-                    ->columns(3),
+                // Personal Information Fields
             ]);
     }
 }
--- app/Models/StartUpYourNewLife.php
+++ app/Models/StartUpYourNewLife.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class StartUpYourNewLife extends Model
 {
-    //
+    protected $table = 'start_up_your_new_life';
+    
+    protected $fillable = [
+        'member_id',
+        'lesson_number',
+        'completion_date',
+        'notes'
+    ];
+
+    protected $casts = [
+        'completion_date' => 'date'
+    ];
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
 }
